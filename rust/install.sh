bbe1f5d0250f4b9d1e65d550659a8a49e1893b6f
Johannes Schickling <schickling.j@gmail.com>
2014-11-10 14:58:45 +0100

    libncurses5-dev

--- rust/install.sh
+++ rust/install.sh
@@ -3,7 +3,7 @@
 triple=x86_64-unknown-linux-gnu
 
 # install curl (needed to install rust)
-apt-get update && apt-get install -y curl gdb g++-multilib lib32stdc++6 libssl-dev
+apt-get update && apt-get install -y curl gdb g++-multilib lib32stdc++6 libssl-dev libncurses5-dev
 
 # install rust
 curl -sL https://static.rust-lang.org/dist/rust-nightly-$triple.tar.gz | tar xvz -C /tmp
