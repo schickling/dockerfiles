da543ef63458eb7fdce7fdf5232d1345a54dbcb7
Johannes Schickling <schickling.j@gmail.com>
2014-11-25 23:11:10 +0100

    step back

--- jekyll/install.sh
+++ jekyll/install.sh
@@ -3,11 +3,11 @@
 # exit if a command fails
 set -e
 
-# install ruby 2.1
+# install ruby 1.9.1
 apt-get update && apt-get install -y ruby ruby-dev build-essential
 
-# install jekyll & bundler (therubyracer needed for coffeescript support)
-gem install jekyll bundler therubyracer --no-ri --no-rdoc
+# install jekyll & bundler (therubyracer needed for coffeescript support, rouge for highlightning)
+gem install jekyll bundler therubyracer rouge --no-ri --no-rdoc
 
 # cleanup package manager
 apt-get remove --purge -y build-essential ruby-dev && apt-get autoclean && apt-get clean
