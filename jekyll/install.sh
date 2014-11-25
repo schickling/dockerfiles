369bd6f2cddc80b607de6753985a0a31a91cd1ec
Johannes Schickling <schickling.j@gmail.com>
2014-11-25 21:08:53 +0100

    coffee

--- jekyll/install.sh
+++ jekyll/install.sh
@@ -6,8 +6,8 @@ set -e
 # install ruby 1.9.1
 apt-get update && apt-get install -y ruby ruby-dev build-essential
 
-# install jekyll
-gem install jekyll bundler --no-ri --no-rdoc
+# install jekyll & bundler (therubyracer needed for coffeescript support)
+gem install jekyll bundler therubyracer --no-ri --no-rdoc
 
 # cleanup package manager
 apt-get remove --purge -y build-essential ruby-dev && apt-get autoclean && apt-get clean
