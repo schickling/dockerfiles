52fc9de0c3d87d55d4fdf57d04b7a054dd9d3861
Johannes Schickling <schickling.j@gmail.com>
2014-11-25 20:53:21 +0100

    make

--- jekyll/install.sh
+++ jekyll/install.sh
@@ -4,13 +4,13 @@
 set -e
 
 # install ruby 1.9.1
-apt-get update && apt-get install -y ruby
+apt-get update && apt-get install -y ruby ruby-dev build-essential
 
 # install jekyll
 gem install jekyll bundler --no-ri --no-rdoc
 
 # cleanup package manager
-apt-get autoclean && apt-get clean
+apt-get remove --purge -y build-essential ruby-dev && apt-get autoclean && apt-get clean
 rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
 
 # prepare dir
