25e2f759e3bc4091e35ada6db5cab5b68888aa67
Gabriel Sobrinho <gabriel.sobrinho@gmail.com>
2015-09-02 13:41:13 -0300

    Remove superfluous parentheses

--- postgres-backup-s3/backup.sh
+++ postgres-backup-s3/backup.sh
@@ -45,7 +45,7 @@ export AWS_DEFAULT_REGION=$S3_REGION
 export PGPASSWORD=$POSTGRES_PASSWORD
 POSTGRES_HOST_OPTS="-h $POSTGRES_HOST -p $POSTGRES_PORT -U $POSTGRES_USER"
 
-echo "Creating dump of ${POSTGRES_DATABASE} database) from ${POSTGRES_HOST}..."
+echo "Creating dump of ${POSTGRES_DATABASE} database from ${POSTGRES_HOST}..."
 
 pg_dump $POSTGRES_HOST_OPTS $POSTGRES_DATABASE | gzip > dump.sql.gz
 
