2ae9f833492876ef33ec96ed34e43bca6d963fb6
Johannes Schickling <schickling.j@gmail.com>
2015-09-15 23:46:22 +0100

    fix s3cmd

--- s3cmd/install.sh
+++ s3cmd/install.sh
@@ -5,7 +5,7 @@ set -e
 
 # install s3cmd
 apk update
-apk add python py-pip py-setuptools git openssl
+apk add python py-pip py-setuptools git ca-certificates
 pip install python-magic
 git clone https://github.com/s3tools/s3cmd.git /tmp/s3cmd
 cd /tmp/s3cmd
