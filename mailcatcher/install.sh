7f14704ba3c03ae2a2da89a19b133ab0254ba2ea
Nicolas Collard <beauceron@dogstudio.be>
2016-04-07 15:07:34 +0200

    Force v0.5.12 to avoid to crash on UTF-8 characters

--- mailcatcher/install.sh
+++ mailcatcher/install.sh
@@ -3,13 +3,13 @@
 # exit if a command fails
 set -e
 
-# install ruby 1.9.1
-apt-get update && apt-get install -y ruby ruby-dev build-essential sqlite3 libsqlite3-dev
+# install dependencies
+apt-get update && apt-get install -y build-essential sqlite3 libsqlite3-dev
 
 # install mailcatcher
-gem install mailcatcher --no-ri --no-rdoc
+gem install mailcatcher -v 0.5.12 --no-ri --no-rdoc
 
 # cleanup package manager
-apt-get remove --purge -y build-essential ruby-dev libsqlite3-dev && apt-get autoclean && apt-get clean
+apt-get remove --purge -y build-essential libsqlite3-dev && apt-get autoclean && apt-get clean
 rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
 
