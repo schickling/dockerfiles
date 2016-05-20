865f8b2798965339c0f103cbb4346d9ae3eaef34
Kieran Brownlees <kieran@kando.io>
2016-05-20 18:24:39 +1200

    postgres-restore-s3
    Pretty basic and is dangerous, but seems to work.
    
    There is an option to drop the current public schema

---
+++ postgres-restore-s3/install.sh
@@ -0,0 +1,17 @@
+#! /bin/sh
+
+# exit if a command fails
+set -e
+
+apk update
+
+# install pg_dump
+apk add 'postgresql>9.5.0'
+
+# install s3 tools
+apk add python py-pip
+pip install awscli
+apk del py-pip
+
+# cleanup
+rm -rf /var/cache/apk/*
