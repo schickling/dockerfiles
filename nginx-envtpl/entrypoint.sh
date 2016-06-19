a46101dcee41f101a16dcf11fca13a6f07af888c
Johannes Schickling <schickling.j@gmail.com>
2016-06-19 17:08:26 +0200

    fix redirect

--- nginx-envtpl/entrypoint.sh
+++ nginx-envtpl/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [[ ! -z $NGINX_CONFIG_TEMPLATE ]]; then
-  envtpl < $NGINX_CONFIG_TEMPLATE > /etc/nginx/nginx.conf
+  echo $NGINX_CONFIG_TEMPLATE | envtpl > /etc/nginx/nginx.conf
 else
   envtpl /etc/nginx/nginx.conf.tpl
 fi
