96e1d095915c905c01acb8dcf6405f71c70b51db
Johannes Schickling <schickling.j@gmail.com>
2016-07-14 11:14:23 +0200

    base64 nginx

--- nginx-envtpl/entrypoint.sh
+++ nginx-envtpl/entrypoint.sh
@@ -2,6 +2,8 @@
 
 if [[ ! -z $NGINX_CONFIG_TEMPLATE ]]; then
   echo $NGINX_CONFIG_TEMPLATE | envtpl > /etc/nginx/nginx.conf
+elif [[ ! -z $NGINX_CONFIG_TEMPLATE_B64 ]]; then
+  echo $NGINX_CONFIG_TEMPLATE_B64 | base64 --decode | envtpl > /etc/nginx/nginx.conf
 else
   envtpl /etc/nginx/nginx.conf.tpl
 fi
