79d58a224e3d2ed4fd36e08d10047772b9307d12
EvgenyOrekhov <EvgenyOrekhov@gmail.com>
2016-08-19 13:52:32 +0300

    Fix #30: make URL to swagger.json relative in swagger-ui/run.sh

--- swagger-ui/run.sh
+++ swagger-ui/run.sh
@@ -9,8 +9,8 @@ if [ "$API_KEY" != "**None**" ]; then
 fi
 
 if [[ -f $SWAGGER_JSON ]]; then
-  sed -i "s|http://petstore.swagger.io/v2/swagger.json|/swagger.json|g" index.html
-  sed -i "s|http://example.com/api|/swagger.json|g" index.html
+  sed -i "s|http://petstore.swagger.io/v2/swagger.json|swagger.json|g" index.html
+  sed -i "s|http://example.com/api|swagger.json|g" index.html
 else
   sed -i "s|http://petstore.swagger.io/v2/swagger.json|$API_URL|g" index.html
   sed -i "s|http://example.com/api|$API_URL|g" index.html
