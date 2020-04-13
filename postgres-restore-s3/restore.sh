6afe55134a9db5f9abaf5fc64dca72308a71e72b
Igor Diakonov <aidos.tanatos@gmail.com>
2020-04-13 18:25:58 +0300

    Fixed quotation

--- postgres-restore-s3/restore.sh
+++ postgres-restore-s3/restore.sh
@@ -68,16 +68,16 @@ gzip -d dump.sql.gz
 
 if [ "${DROP_PUBLIC}" == "yes" ]; then
 	echo "Recreating the public schema"
-	psql $POSTGRES_HOST_OPTS -d $POSTGRES_DATABASE -c "drop schema public cascade; create schema public;"
+	psql $POSTGRES_HOST_OPTS -d "$POSTGRES_DATABASE" -c "drop schema public cascade; create schema public;"
 fi
 
 echo "Restoring ${LATEST_BACKUP}"
 
 psql $POSTGRES_HOST_OPTS -d postgres -c "SELECT pg_terminate_backend(pg_stat_activity.pid) FROM pg_stat_activity WHERE pg_stat_activity.datname = '$POSTGRES_DATABASE' AND pid <> pg_backend_pid();"
-psql $POSTGRES_HOST_OPTS -d postgres -c "DROP DATABASE IF EXISTS $POSTGRES_DATABASE;"
-psql $POSTGRES_HOST_OPTS -d postgres -c "CREATE DATABASE $POSTGRES_DATABASE;"
-psql $POSTGRES_HOST_OPTS -d postgres -c "GRANT ALL PRIVILEGES ON DATABASE $POSTGRES_DATABASE TO $POSTGRES_USER;"
-pg_restore $POSTGRES_HOST_OPTS --no-owner -j $RESTORE_JOBS -v -d $POSTGRES_DATABASE dump.sql
+psql $POSTGRES_HOST_OPTS -d postgres -c "DROP DATABASE IF EXISTS \"$POSTGRES_DATABASE\";"
+psql $POSTGRES_HOST_OPTS -d postgres -c "CREATE DATABASE \"$POSTGRES_DATABASE\";"
+psql $POSTGRES_HOST_OPTS -d postgres -c "GRANT ALL PRIVILEGES ON DATABASE \"$POSTGRES_DATABASE\" TO $POSTGRES_USER;"
+pg_restore $POSTGRES_HOST_OPTS --no-owner -j $RESTORE_JOBS -v -d "$POSTGRES_DATABASE" dump.sql
 
 echo "Restore complete"
 
