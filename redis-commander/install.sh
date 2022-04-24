10d1454b7881c9b15efb79c227cc630163318289
Jérémy DECOOL <contact@jdecool.fr>
2022-04-24 08:38:41 +0200

    Upgrade redis-commander image to debian:bullseye

--- redis-commander/install.sh
+++ redis-commander/install.sh
@@ -6,9 +6,6 @@ set -eo pipefail
 # install node & npm
 apt-get update && apt-get install -y nodejs npm
 
-# alias as node
-ln -sf /usr/bin/nodejs /usr/bin/node
-
 # install redis commander
 npm install -g redis-commander
 
