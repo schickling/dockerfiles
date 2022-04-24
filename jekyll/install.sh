525fb91e05bddaac727b34eef5908b9713e4afb2
Jérémy DECOOL <contact@jdecool.fr>
2022-04-24 08:36:56 +0200

    Upgrade jekyll image to debian:bullseye

--- jekyll/install.sh
+++ jekyll/install.sh
@@ -4,10 +4,10 @@
 set -eo pipefail
 
 # install ruby 1.9.1
-apt-get update && apt-get install -y ruby ruby-dev build-essential
+apt-get update && apt-get install -y python2 ruby ruby-dev build-essential
 
 # install jekyll & bundler (therubyracer needed for coffeescript support, rouge for highlightning)
-gem install jekyll bundler therubyracer rouge --no-ri --no-rdoc
+gem install jekyll bundler therubyracer rouge --no-document
 
 # cleanup package manager
 apt-get remove --purge -y build-essential ruby-dev && apt-get autoclean && apt-get clean
