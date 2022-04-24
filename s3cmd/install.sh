5f0f694bae22aab0ecc4a6656377d92ce8f8ea2d
Jérémy DECOOL <contact@jdecool.fr>
2022-04-24 08:39:15 +0200

    Fix s3cmd image build

--- s3cmd/install.sh
+++ s3cmd/install.sh
@@ -5,11 +5,11 @@ set -eo pipefail
 
 # install s3cmd
 apk update
-apk add --no-cache python py-pip py-setuptools git ca-certificates
+apk add --no-cache python3 py-pip py-setuptools git ca-certificates
 pip install python-magic
 git clone https://github.com/s3tools/s3cmd.git /tmp/s3cmd
 cd /tmp/s3cmd
-python setup.py install
+python3 setup.py install
 apk del git
 rm -rf /tmp/s3cmd
 
