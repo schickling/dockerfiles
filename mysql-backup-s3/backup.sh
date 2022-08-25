4302b2a5d3dd38de0a3c865c864e85650cbca5f7
Matias Garcia Isaia <mgarcia@manas.tech>
2022-08-25 15:02:11 -0300

    Allow to opt-out of S3 bucket creation in mysql-backup
    This avoids the need to grant S3 list/read access to the backup user

--- mysql-backup-s3/backup.sh
+++ mysql-backup-s3/backup.sh
@@ -50,11 +50,13 @@ copy_s3 () {
     AWS_ARGS="--endpoint-url ${S3_ENDPOINT}"
   fi
 
-  echo "Ensuring S3 bucket $S3_BUCKET exists"
-  EXISTS_ERR=`aws $AWS_ARGS s3api head-bucket --bucket "$S3_BUCKET" 2>&1 || true`
-  if [[ ! -z "$EXISTS_ERR" ]]; then
-    echo "Bucket $S3_BUCKET not found (or owned by someone else), attempting to create"
-    aws $AWS_ARGS s3api create-bucket --bucket $S3_BUCKET
+  if [ "${S3_ENSURE_BUCKET_EXISTS}" != "no" ]; then
+    echo "Ensuring S3 bucket $S3_BUCKET exists"
+    EXISTS_ERR=`aws $AWS_ARGS s3api head-bucket --bucket "$S3_BUCKET" 2>&1 || true`
+    if [[ ! -z "$EXISTS_ERR" ]]; then
+      echo "Bucket $S3_BUCKET not found (or owned by someone else), attempting to create"
+      aws $AWS_ARGS s3api create-bucket --bucket $S3_BUCKET
+    fi
   fi
 
   echo "Uploading ${DEST_FILE} on S3..."
