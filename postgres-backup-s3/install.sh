b3c3cedc4bc4bbe58f9e8d9817fdea3fa258c28f
Anik Ghosh <52236930+anikghosh256@users.noreply.github.com>
2024-10-16 14:47:14 +0600

    chore(postgres-backup-s3): update postgresql-client version

--- postgres-backup-s3/install.sh
+++ postgres-backup-s3/install.sh
@@ -5,7 +5,7 @@ set -eo pipefail
 
 apk update
 apk add openssl aws-cli 
-apk add postgresql-client --repository=https://dl-cdn.alpinelinux.org/alpine/v3.18/main
+apk add postgresql-client --repository=https://dl-cdn.alpinelinux.org/alpine/v3.19/main
 
 # cleanup
 rm -rf /var/cache/apk/*
